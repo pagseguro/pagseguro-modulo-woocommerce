1776134c52dea38b959dbae38b8fd2deeca199f3
andresilvas2it <andre.silva@s2it.com.br>
2013-07-08 16:29:59 -0300

    comit módulo woocommerce completo

---
+++ wordpresswoocommercepagseguro/classes/wpwcmodalpagseguro.class.php
@@ -0,0 +1,175 @@
+<?php
+/*
+************************************************************************
+Copyright [2013] [PagSeguro Internet Ltda.]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+************************************************************************
+*/
+
+/**
+ * Class use db for PagSeguro
+ */
+class WP_WC_Modal_Pagseguro {
+    
+    /**
+     * Array with PagSeguro status
+     * 
+     * @var array 
+     */
+    private static $array_order_status = array(
+        'en_US' => array(
+	0 => array(0=>'Pending',1=>'Pending'),
+        1 => array(0=>'Awaiting payment',1=>'Awaiting_Payment'),
+        2 => array(0=>'Processing',1=>'Processing'),
+        3 => array(0=>'Paid',1=>'Paid'),
+        4 => array(0=>'Complete',1=>'Complete'),
+        5 => array(0=>'Dispute',1=>'Dispute'),
+        6 => array(0=>'Refunded',1=>'Refunded'),
+        7 => array(0=>'Canceled',1=>'Canceled')),
+        
+        'pt_BR' => array(
+        0 => array(0=>'Iniciada',1=>'Iniciada'),
+        1 => array(0=>'Aguardando pagamento',1=>'Aguardando_Pagamento'),
+        2 => array(0=>'Em análise',1=>'Em_Análise'),
+        3 => array(0=>'Paga',1=>'Paga'),
+        4 => array(0=>'Disponível',1=>'Disponível'),
+        5 => array(0=>'Em disputa',1=>'Em_Disputa'),
+        6 => array(0=>'Devolvida',1=>'Devolvida'),
+        7 => array(0=>'Cancelada',1=>'Cancelada'))
+        );
+    
+    /**
+     * Save PagSeguro status not registered to locale
+     */
+    public function saveOrderStatus(){
+        $this->runsOrderStatus('pt_BR');
+        $this->runsOrderStatus('en_US');
+    }
+    
+    /**
+     * Save PagSeguro status not registered
+     * 
+     * @global type $wpdb
+     * @param type $locale
+     */
+    private function runsOrderStatus($locale){
+        global $wpdb;
+        
+        foreach (self::$array_order_status[$locale] as $key => $value){
+            $term = $wpdb->get_row( $wpdb->prepare( "SELECT t.* FROM $wpdb->terms AS t WHERE t.name = '".trim($value[0])."'") );
+            if ( !$term ){
+                $wpdb->query("INSERT INTO $wpdb->terms (name, slug, term_group) VALUES ('".trim($value[0])."','".trim($value[1])."', 0)");
+
+                $term_id = $wpdb->get_var($wpdb->prepare("SELECT term_id FROM $wpdb->terms WHERE name = '".trim($value[0])."'"));
+
+                $wpdb->query("INSERT INTO $wpdb->term_taxonomy (term_id, taxonomy, description, parent, count) VALUES ($term_id,'shop_order_status','',0,0)");
+            }
+        }
+    }
+    
+    /**
+     * Use for get $array_order_status
+     * 
+     * @return Array 
+     */
+    public function getOrderStatus($locale){
+        return self::$array_order_status[$locale];
+    }
+    
+    /**
+     * 
+     * Return Key from Order Status for Name
+     * 
+     * @global type data variable
+     * @param type String
+     * @return type Integer
+     */
+    public function getKeyOrderStatusByName($name){
+        global $wpdb;
+        $term_id = NULL;
+        
+        if(isset($name) && !empty($name)){
+            $term_id = $wpdb->get_var($wpdb->prepare("SELECT term_id FROM $wpdb->terms WHERE name LIKE '".trim($name)."'"));
+            return !empty($term_id) ? $term_id : NULL;
+        }
+        
+        return $term_id;
+    }
+    
+    /**
+     * 
+     * Return Name from Order Status for Key
+     * 
+     * @global type type data variable
+     * @param type String
+     * @return type Integer
+     */
+    public function getNameOrderStatusByKey($key){
+        global $wpdb;
+        $term_name = NULL;
+        
+        if(isset($key) && !empty($key)){
+            $term_name = $wpdb->get_var($wpdb->prepare("SELECT name FROM $wpdb->terms WHERE term_id = $key"));
+            return !empty($term_name) ? $term_name : NULL;
+        }
+        
+        return $term_name;
+    }
+    
+    /**
+     * Update status in table wp_term_relationships
+     * 
+     * @param type $order_id, reference order
+     * @param type $term_id, id status
+     */
+    public function updateOrder($order_id, $term_id){
+        global $wpdb;
+        
+        $term_taxonomy_id = $wpdb->get_var($wpdb->prepare("SELECT term_taxonomy_id FROM $wpdb->term_taxonomy WHERE term_id = $term_id"));
+        $term_taxonomy_id_relation = $wpdb->get_var($wpdb->prepare("SELECT term_taxonomy_id FROM $wpdb->term_relationships WHERE object_id = $order_id"));
+        if($term_taxonomy_id == $term_taxonomy_id_relation){
+            return false;
+        }else{
+            $wpdb->query("UPDATE $wpdb->term_relationships SET term_taxonomy_id = $term_taxonomy_id WHERE object_id = $order_id;");
+            return true;
+        }
+    }
+    
+    /**
+     * Save Historic when change PagSeguro status
+     * 
+     * @global type $wpdb
+     * @param type $order_id
+     * @param type $name_order_status
+     * @param type $update
+     */
+    public function saveHistoric($order_id,$name_order_status,$update){
+        if($update){
+            global $wpdb;
+
+            $comment_post_ID 		= $order_id;
+            $comment_author 		= 'PagSeguro';
+            $comment_author_url 	= '';
+            $comment_content 		= sprintf( __( 'Status updated to %s', 'wpwcpagseguro' ), $name_order_status );
+            $comment_agent              = 'WooCommerce';
+            $comment_type		= 'order_note';
+            $comment_parent		= 0;
+            $comment_approved 		= 1;
+            $commentdata 			= compact( 'comment_post_ID', 'comment_author', 'comment_author_email', 'comment_author_url', 'comment_content', 'comment_agent', 'comment_type', 'comment_parent', 'comment_approved' );
+
+            wp_insert_comment( $commentdata );
+        }
+    }
+}
+?>
