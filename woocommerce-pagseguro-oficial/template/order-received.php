3615738cd827e63538bbade8f41e23bed5b3afaa
Nicolas C. Barbosa <nicolascb.claudio@gmail.com>
2016-12-24 17:50:10 -0200

    layout order confirmation screen

--- woocommerce-pagseguro-oficial/template/order-received.php
+++ woocommerce-pagseguro-oficial/template/order-received.php
@@ -1,48 +1,41 @@
 <section class="checkout-success">
     <h1 class="title-detail">Detalhes do Pedido</h1>
-    <h2 class="title-order">Muito obrigado pela sua compra!!</h2>
     <?php
         $order = wc_get_order($_REQUEST['order_id']);
-        print_r($order);
+//        print_r($order);
         $public_view_order_url = esc_url( $order->get_view_order_url() );
         $shop_url = get_site_url();
     ?>
+    <div class="order-info">
+        <h2 class="title text-center">Seu pedido foi efetuado com sucesso</h2>
+        <p class="item">
+            <span class="info-label">Data e Hora:</span>
+            <span class="info"><?= $order->order_date ?></span>
+        </p>
+    </div>
     <?php if ($_REQUEST['checkout_type'] == 'boleto'): ?>
-        <div class="actions-toolbar">
-            <a class="action-order" href="<?= $public_view_order_url?>">
-                Ver pedido gerado
-            </a>
-            <a class="action-order" href="<?= $shop_url?>">
-                Continue comprando
-            </a>
-            <p class="text-or">Ou</p>
-            <a class="ps-button " target="_blank" href="<?= $_REQUEST['payment_link'];?>">
+        <div class="text-center">
+            <a class="btn-action-order -center" target="_blank" href="<?= $_REQUEST['payment_link'];?>">
                 Imprimir boleto
             </a>
         </div>
     <?php endif; ?>
 
     <?php if ($_REQUEST['checkout_type'] == 'debit'): ?>
-        <div class="actions-toolbar">
-            <a class="ps-button" target="_blank" href="<?= $_REQUEST['payment_link'];?>">
+        <div class="text-center">
+            <a class="btn-action" target="_blank" href="<?= $_REQUEST['payment_link'];?>">
                 Ir para o site do banco
-            </a>
-            <p class="text-or">Ou</p>
-            <a class="action-order" href="<?= $shop_url?>">
-                Continue comprando
             </a>
         </div>
     <?php endif; ?>
 
-    <?php if ($_REQUEST['checkout_type'] == 'credit-card'): ?>
-        <div class="actions-toolbar">
-            <a class="action-order" href="<?= $public_view_order_url?>">
-                Ver pedido gerado
-            </a>
-            <p class="text-or">Ou</p>
-            <a class="action-order" href="<?= $shop_url?>">
-                Continue comprando
-            </a>
-        </div>
-    <?php endif; ?>
+    <p class="text-or">Ou</p>
+    <div class="action-bar clearfix">
+        <a class="btn-action-order" href="<?= $public_view_order_url?>">
+            Detalhes do pedido
+        </a>
+        <a class="btn-action-order" href="<?= $shop_url?>">
+            Continue comprando
+        </a>
+    </div><!-- /.actions--bar -->
 </section>
