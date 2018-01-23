a39f57127e380b1833aaa54be925406ac3d1790b
Thiago-Medeiros <dev.thiagomedeiros@gmail.com>
2018-01-22 22:29:45 -0200

    refactor: get order date by getter
    fix: check for url params

--- template/order-received.php
+++ template/order-received.php
@@ -1,17 +1,17 @@
 <section class="ps-wrap checkout-success">
     <h1 class="title-detail">Detalhes do Pedido</h1>
     <?php
-        $order = wc_get_order($_REQUEST['order_id']);
-        $order_number = $order->get_order_number();
-        //Products Info
-        $order_items = $order->get_items();
+    $order = wc_get_order($_REQUEST['order_id']);
+    $order_number = $order->get_order_number();
+    //Products Info
+    $order_items = $order->get_items();
 
-        //Data
-        $date_request = new DateTime($order->order_date);
+    //Data
+    $date_request = new DateTime($order->get_date_completed());
 
-        //Links to buttons
-        $public_view_order_url = esc_url( $order->get_view_order_url() );
-        $shop_url = get_permalink( get_option( 'woocommerce_shop_page_id' ) );
+    //Links to buttons
+    $public_view_order_url = esc_url( $order->get_view_order_url() );
+    $shop_url = get_permalink( get_option( 'woocommerce_shop_page_id' ) );
     ?>
     <div class="order-info">
         <h2 class="title text-center">Seu pedido foi efetuado com sucesso</h2>
@@ -29,44 +29,44 @@
         </p>
         <table>
             <thead>
-                <th>
-                   Produto
-                </th>
-                <th>
-                    Valor
-                </th>
+            <th>
+                Produto
+            </th>
+            <th>
+                Valor
+            </th>
             </thead>
             <tbody>
             <?php
-                foreach ($order_items as $item) {
-                    $product_name = $item['name'];
-            ?>
-             <tr>
-                 <td>
-                     <?= $item['name'] ?>
-                 </td>
-                 <td>
-                     R$ <?= $item['line_total'] ?>
-                 </td>
-             </tr>
-            <?php
-                }
+            foreach ($order_items as $item) {
+                $product_name = $item['name'];
+                ?>
+                <tr>
+                    <td>
+                        <?= $item['name'] ?>
+                    </td>
+                    <td>
+                        R$ <?= $item['line_total'] ?>
+                    </td>
+                </tr>
+                <?php
+            }
             ?>
             </tbody>
             <tfoot>
-                <tr>
-                    <th>
-                        Total
-                    </th>
-                    <td class="total-price">
-                        R$ <?= $order->get_total()?>
-                    </td>
-                </tr>
+            <tr>
+                <th>
+                    Total
+                </th>
+                <td class="total-price">
+                    R$ <?= $order->get_total()?>
+                </td>
+            </tr>
 
             </tfoot>
         </table>
     </div>
-    <?php if ($_REQUEST['link_boleto']): ?>
+    <?php if (isset($_REQUEST['link_boleto']) && $_REQUEST['link_boleto']): ?>
         <div class="text-center">
             <p>Se a página de impressão do boleto não abriu, clique no link abaixo</p>
             <a class="btn-action-order -center" target="_blank" href="<?= $_REQUEST['link_boleto'];?>">
@@ -74,7 +74,7 @@
             </a>
         </div>
     <?php endif; ?>
-    <?php if ($_REQUEST['link_debit']): ?>
+    <?php if (isset($_REQUEST['link_debit']) && $_REQUEST['link_debit']): ?>
         <div class="text-center">
             <p>Se a tela de pagamento não abriu clique no link abaixo</p>
             <a class="btn-action-order -center" target="_blank" href="<?= $_REQUEST['link_debit'];?>">
@@ -83,15 +83,15 @@
         </div>
     <?php endif; ?>
 
-	<?php if ($_REQUEST['checkout_type'] == 'boleto'): ?>
+    <?php if (isset($_REQUEST['checkout_type']) && $_REQUEST['checkout_type'] == 'boleto'): ?>
         <div class="text-center">
             <a class="btn-action-order -center" target="_blank" href="<?= $_REQUEST['payment_link'];?>">
                 Imprimir boleto
             </a>
         </div>
-	<?php endif; ?>
+    <?php endif; ?>
 
-    <?php if ($_REQUEST['checkout_type'] == 'debit'): ?>
+    <?php if (isset($_REQUEST['checkout_type']) && $_REQUEST['checkout_type'] == 'debit'): ?>
         <div class="text-center">
             <a class="btn-action-order" target="_blank" href="<?= $_REQUEST['payment_link'];?>">
                 Ir para o site do banco
